01d2c3c15f4acf598fd792b833e3ba1d50ed82bc
Sania1405 <Sania14b5@gmail.com>
2025-05-10 18:46:33 +0530

    comments added

--- linkedlist/impusingSTL.cpp
+++ linkedlist/impusingSTL.cpp
@@ -9,20 +9,20 @@ int main(){
     ll.push_back(30);
     ll.push_back(40);
     ll.push_back(50);
-    ll.push_back(60);
+    ll.push_back(60); //10 20 30 40 50 60
     ll.push_front(70);
-    ll.push_front(80);
+    ll.push_front(80); //80 70 10 20 30 40 50 60
     for(int val:ll){
         cout<<val<<"--> ";
     }
-    cout<<"NULL"<<endl;
-    cout<<"size :"<<ll.size()<<endl;
-    cout<<"front val:"<<ll.front()<<endl;
-    cout<<"back val:"<<ll.back()<<endl;
-    ll.pop_front();
+    cout<<"NULL"<<endl;      
+    cout<<"size :"<<ll.size()<<endl;  //8
+    cout<<"front val:"<<ll.front()<<endl;   //80
+    cout<<"back val:"<<ll.back()<<endl;  //60
+    ll.pop_front();    
     ll.pop_back();
     for(int val:ll){
-        cout<<val<<"--> ";
+        cout<<val<<"--> ";            // 70 10 20 30 40 50 
     }
     cout<<"NULL"<<endl;
 }
