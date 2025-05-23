71bd3e57abf5b9fa80202ec385a414ea19e41005
Sania1405 <Sania14b5@gmail.com>
2025-05-23 12:40:22 +0530

    tree traversals

--- Tree/built_tree_preorder.cpp
+++ Tree/built_tree_preorder.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 class Node{
     public:
@@ -46,7 +47,64 @@ void inorder(Node*root){
     cout<<root->data<<" ";
     inorder(root->right);
 }
-
+void levelorder1(Node*root){
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        Node*curr=q.front();
+        q.pop();
+        if(curr!=NULL){
+            cout<<curr->data<<" ";  //it will give output as 1 2 3 4 5 6
+            if(curr->left!=NULL){
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL){
+                q.push(curr->right);
+            }
+        }
+    }
+}
+void levelorder2(Node*root){
+    queue<Node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node*curr=q.front();
+        q.pop();
+        if(curr==NULL){
+            cout<<endl;
+            if(q.empty()){
+                break;
+            }
+            q.push(NULL);
+        }else{
+            cout<<curr->data<<" "; 
+            if(curr->left!=NULL){
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL){
+                q.push(curr->right);
+            }
+        }
+    }
+}
+int levelordersum(Node*root){
+    queue<Node*>q;
+    int result=0;
+    q.push(root);
+    while(!q.empty()){
+        Node*curr=q.front();
+        q.pop();
+        result+=curr->data;
+        if(curr->left!=NULL){
+            q.push(curr->left);
+        }
+        if(curr->right!=NULL){
+            q.push(curr->right);
+        }         //it will give the sum of all nodes ....
+    }
+    return result;
+}
 int main(){
     vector<int>node={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Node*root=buildtree(node);
@@ -60,5 +118,13 @@ int main(){
     cout<<endl;
     cout<<"inorder traversal: ";
     inorder(root);
+    cout<<endl;
+    cout<<"level order traversal: ";
+    levelorder1(root);
+    cout<<endl;
+    cout<<"level order traversal: ";
+    levelorder2(root);
+    cout<<"level order sum: ";
+    cout<<levelordersum(root);
     return 0;
 }
